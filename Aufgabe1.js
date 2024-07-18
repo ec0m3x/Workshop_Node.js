3870412b49d554d5e80759544d432a17d527a4f4
ecomex <skochsks-concept.de>
2024-07-18 15:07:20 +0200

    1 and 2 done

--- Aufgabe1.js
+++ Aufgabe1.js
@@ -1,23 +1,33 @@
-// https-Modul importieren
+// Import the 'https' module
+const https = require('https');
 
-// express-Modul importieren
+// Import the 'express' module
+const express = require('express');
 
 const selfsigned = require('selfsigned');
 
 
 const attrs = [{name: 'commonName', value: 'localhost'}];
-const pems = selfsigned.generate(attrs, {days: 1});
+const pems = selfsigned.generate(attrs, { days: 1, keySize: 2048 });
 
 //Erstelle eine Express-Anwendung
 
+const app = express();
 
 //Definiere eine einfache Route
 
+app.get('/', (req, res) => {
+    res.send('What the hell happened?');
+});
 
 https.createServer({
      //Privaten Schlüssel übergeben
+        key: pems.private,
     // Zertifikat übergeben
+        cert: pems.cert
+
+
 }, app).listen(3000, () => {
     //Ausgabe des Servers
-    
+    console.log('Server is running on port 3000');
 });
--- Aufgabe2.js
+++ Aufgabe2.js
@@ -3,9 +3,9 @@
 //Diese wird nach der langsamen Operation fertig
 
 //EventEmitter Modul importieren
-
+const EventEmitter = require('events');
 //Neues EventEmitter Objekt anlegen
-
+const eventEmitter = new EventEmitter();
 
 const slowOperation = () => {
     console.log('Starte langsame Operation...');
@@ -13,7 +13,8 @@ const slowOperation = () => {
     setTimeout(() => {
         const result = 'Langsame Operation abgeschlossen!';
         //Event auslösen, wenn langsame Operation abgeschlossen ist
-        
+        eventEmitter.emit('operationComplete', result);
+
     }, 3000);
 };
 
@@ -23,18 +24,23 @@ const additionalTask = () => {
     setTimeout(() => {
         const result = 'Zusätzliche Aufgabe abgeschlossen!';
         //Event auslösen, wenn zusätzliche Aufgabe abgeschlossen ist
-        
+        eventEmitter.emit('additonalTaskComplete', result);
+
     }, 4000);
 };
 
 //EreignisListener, für das Ereignis "operationComplete", mit Übergabe der Variable result
- => {
+eventEmitter.on('operationComplete', (result) => {
     console.log(result);
+    //Starte zusätzliche Aufgabe
+    additionalTask();
 });
 //EreignisListener, für das Ereignis "additonalTaskComplete", mit Übergabe der Variable result
- => {
+eventEmitter.on('additonalTaskComplete', (result) => {
     console.log(result);
 });
 
 //Start des Hauptprogramms und der weiteren Aufgaben
+slowOperation();
+console.log('Hauptprogramm läuft weiter...');
 
